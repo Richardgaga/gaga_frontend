0a28da72952127754a062f6e36f6bc53094cea0d
Richardgaga <herokugaza@gmail.com>
2026-04-01 17:19:54 +0300

    Update chart.js

--- src/chart.js
+++ src/chart.js
@@ -1,6 +1,6 @@
-# Candlestick chart
 import React from "react";
-import { Chart as ChartJS } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
 
 function Chart() {
   const data = {
@@ -10,11 +10,12 @@ function Chart() {
         label: "EUR/USD Candles",
         data: [1.15, 1.16, 1.17],
         borderColor: "blue",
+        backgroundColor: "lightblue",
       }
     ]
   };
 
-  return <ChartJS type="line" data={data} />;
+  return <Line data={data} />;
 }
 
 export default Chart;
